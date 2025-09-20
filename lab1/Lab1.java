7e5037eef5c62354acf9afebadd7e09998962ae4
LeRenard314 <th3ph3n0m3naldud3@gmail.com>
2025-09-20 21:23:44 +0300

    Modified logic entirely, method durakProtocol not ready yet

--- lab1/Lab1.java
+++ lab1/Lab1.java
@@ -1,37 +1,56 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Lab1 {
-
     public static void main(String[] args) {
-        Scanner myScan = new Scanner(System.in);
-        
-        System.out.println("Enter A:");
-        int a = myScan.nextInt();
+        Scanner consoleScanner = new Scanner(System.in);
 
-        System.out.println("Enter B:");
-        int b = myScan.nextInt();
+        HashMap<String, Integer> system1 = buildHashMap(consoleScanner), system2 = buildHashMap(consoleScanner);
 
-        System.out.println("Enter C:");
-        int c = myScan.nextInt();
+        systemComaprison(system1.get("intersectionPoints"), system2.get("intersectionPoints"));
 
-        System.out.println("Enter x0:");
-        int xZero = myScan.nextInt();
-
-        System.out.println("Enter y0:");
-        int yZero = myScan.nextInt();
-
-        System.out.println("Enter R:");
-        int r = myScan.nextInt();
-
-        int distance = Math.abs(a * xZero + b * yZero + c) / (int)Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
-        if (distance > r) {
-            System.out.println("There are no points of connection");
-        } else if (distance == r) {
-            System.out.println("There is 1 point of connection");
-        } else {
-            System.out.println("There are 2 connection points");
-        }
+        consoleScanner.close();
     }
 
+    private static HashMap<String, Integer> buildHashMap(Scanner scan) {
+        String[] coefficients = {"A", "B", "C", "xZero", "yZero", "R"};
+        HashMap<String, Integer> mapData = new HashMap<>();
+
+        System.out.println("The program will prompt you to enter the coefficients of linear and circle equasions");
+        
+        for (String coefficient: coefficients) {
+            System.out.format("Enter %s: ", coefficient);
+            mapData.put(coefficient, scan.nextInt());
+        }
+
+        mapData.put("intersectionPoints", calculateIntersectionPoints(mapData));
+        
+        return mapData;
+    }
+
+    private static boolean durakProtocol(String target, int value) {
+        return false;
+    }
+
+    private static int calculateIntersectionPoints(HashMap<String, Integer> map) {
+        int a = map.get("A"), b = map.get("B"), c = map.get("C"), xZero = map.get("xZero"), yZero = map.get("yZero"), r = map.get("R");
+        int points = 0, distance = Math.abs(a * xZero + b * yZero + c) / (int)Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        if (distance == r) {
+            points = 1;
+        } else if (distance < r) {
+            points = 2;
+        }
+        return points;
+    }
+
+    private static void systemComaprison(int points1, int points2) {
+        if (points1 > points2) {
+            System.out.println("The first system has more intersection points than the second system");
+        } else if (points1 < points2) {
+            System.out.println("The first system has less intersection points than the second system");
+        } else {
+            System.out.println("The first system has the same amount of intersection points as the second system");
+        }
+        System.out.format("Intersection points:\n System 1: %d\t System2: %d\n", points1, points2);
+    }
 }
